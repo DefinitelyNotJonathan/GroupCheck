1468eab6e4c6a3033fb26f81cff5f45ff35eacf6
Jonathan <jonretchless@gmail.com>
2020-03-09 20:26:35 -0700

    home page added to package.json

---
+++ build/precache-manifest.9ce774f1b120c2b77767bef7692787a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020b9c21ff89308534c633512b04472c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0705f012b498821961e",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "16436b70b99bfb041436",
+    "url": "./static/js/2.8028bb40.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.8028bb40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0705f012b498821961e",
+    "url": "./static/js/main.02fe1aac.chunk.js"
+  },
+  {
+    "revision": "0405e3db47deb25ac4cc",
+    "url": "./static/js/runtime-main.989041b0.js"
+  }
+]);
